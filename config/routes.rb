d05ef7d241ee0f6bce1ff22d6bd622d07b137a5f
devnehag <gupta.neha2004@gmail.com>
2024-02-21 02:27:37 -0800

    define routes for users,jobs, other controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    # Users
+  resources :users, only: [:create, :index, :show] do
+    # Additional user-specific routes if needed
+  end
+  # Jobs
+  resources :jobs do
+    resources :checklists, only: [:show] do
+      resources :checklist_items, only: [:index, :create, :update, :destroy]
+    end
+  end
+  # Checklists
+  resources :checklists, only: [:create, :update, :destroy] do
+    resources :checklist_items, only: [:index, :create, :update, :destroy]
+  end
+  # Checklist Items
+  resources :checklist_items, only: [:index, :create, :update, :destroy]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+   # Root route
+   root 'home#index'
 end
